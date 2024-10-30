4f5f7fb787aefa68b3f7aa278225fd78bf89373d
Amauri1327 <amauri1816@hotmail.com>
2024-10-30 13:56:11 -0300

    Primeiro commit

---
+++ app/index.jsx
@@ -0,0 +1,17 @@
+import { Image, StyleSheet, View, Text, Platform } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
