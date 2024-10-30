50bb541a1d94ea3f8a976612ad3d52978d1632f5
Amauri1327 <amauri1816@hotmail.com>
2024-10-30 16:20:02 -0300

    adicao nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Image, StyleSheet, View, Text, Platform } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center">
       <Text>Hello world</Text>
     </View>
   );
