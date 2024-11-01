2a0f66ebe159b063c17b983a70f37e2d36116835
Amauri1327 <amauri1816@hotmail.com>
2024-11-01 14:41:22 -0300

    affirmation index screen

--- app/(tabs)/affirmations.tsx
+++ app/(tabs)/affirmations.tsx
@@ -1,12 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import AppGradient from "@/components/AppGradient";
+import AFFIRMATION_GALLERY from "@/constants/affirmation-gallery";
+import GuidedAffirmationsGallery from "@/components/GuidedAffirmationsGallery";
 
 const Affirmations = () => {
   return (
-    <View>
-      <Text>Affirmations</Text>
+    <View className="flex-1">
+      <AppGradient colors={["#2e1f58", "#54426b", "#a790af"]}>
+        <ScrollView className="mt-10" showsVerticalScrollIndicator={false}>
+          <Text className="text-zinc-50 text-3xl font-bold">
+            Change you beliefs with affimations
+          </Text>
+          <View>
+            {AFFIRMATION_GALLERY.map((g) => (
+              <GuidedAffirmationsGallery
+                key={g.title}
+                title={g.title}
+                previews={g.data}
+              />
+            ))}
+          </View>
+        </ScrollView>
+      </AppGradient>
     </View>
-  )
-}
+  );
+};
 
-export default Affirmations
+export default Affirmations;
